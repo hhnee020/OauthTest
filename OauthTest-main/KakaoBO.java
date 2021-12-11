cdceefc375bf123e8b9b6e8ae0386504c2cc52ac
hhnee020 <hhnee020@gmail.com>
2021-12-11 16:50:35 +0900

    OAuth , 소셜로그인

---
+++ OauthTest-main/KakaoBO.java
@@ -0,0 +1,37 @@
+package egov.bo;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class KakaoBO {
+	
+	private String client_id;
+	private String callbackURL;
+	private String apiURL;
+	
+	public KakaoBO() throws UnsupportedEncodingException {
+		this.client_id = "Your Client iD"; //JavaScript Key ( App key )
+		this.callbackURL = URLEncoder.encode("http://localhost:8090/kakaoCallback.do","UTF-8");
+		this.apiURL = "https://kauth.kakao.com/oauth/authorize?"+
+						"client_id=" + client_id +
+						"&redirect_uri=" +callbackURL +
+						"&response_type=code";
+				
+	}
+	
+	public String getClient_id() {
+		
+		return client_id;
+	}
+	public String getCallbackURL() {
+		return callbackURL;
+	}
+	public String getApiURL() {
+		return apiURL;
+	}
+
+/*	https://kauth.kakao.com/oauth/authorize
+	    ?client_id=730975601d99f3b911f8fb8fff4edafa
+	    &redirect_uri=http://localhost:8080/login
+	    &response_type=code""; */
+}
---
+++ OauthTest-main/README.md
@@ -0,0 +1,18 @@
+# OauthTest
+Naver and Kakao
+
+------------------------------Naver------------------------------
+
+어플리케이션 신청
+https://developers.naver.com/apps/#/register?api=nvlogin
+
+로그인 명세 API
+https://developers.naver.com/docs/login/api/api.md
+
+프로필 조회 명세 API
+https://developers.naver.com/docs/login/profile/profile.md
+
+
+pom.xml 추가
+
+https://mvnrepository.com/artifact/com.google.code.gson/gson/2.8.9
